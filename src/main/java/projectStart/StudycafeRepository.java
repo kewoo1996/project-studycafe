423a727f0fe32fa9fcb5770cf2df0043b5ac27f3
wke <kewoo1996@naver.com>
2020-05-13 22:14:22 +0900

    commit

---
+++ src/main/java/projectStart/StudycafeRepository.java
@@ -0,0 +1,8 @@
+package projectStart;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface StudycafeRepository extends PagingAndSortingRepository<Studycafe, Long>{
+
+
+}
